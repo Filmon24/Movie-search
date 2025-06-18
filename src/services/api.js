72a69a2b841e6d3ae7e186c946d5a97ff61d3ade
Filmon24 <filmonataklty@gmail.com>
2025-06-17 20:49:23 -0700

    previous

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_KEY = "592f0bc405c59d61b1bda7fc1b4e482d";
 const BASE_URL = "https://api.themoviedb.org/3"
 
 export const getPopularMovies = async () => {
